ddf932ee0061935ba11449da6a7f47be77bb11a6
Egor Dashkovskii <e.dashkovskii1@g.nsu.ru>
2025-09-14 17:40:00 +0700

    fix5 codestyle

--- Task_1_1_2/src/test/java/ru/nsu/dashkovskii/BlackjackTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/dashkovskii/BlackjackTest.java
@@ -1,8 +1,8 @@
 package ru.nsu.dashkovskii;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
 
 class BlackjackTest {
 
--- Task_1_1_2/src/test/java/ru/nsu/dashkovskii/PlayerDealerTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/dashkovskii/PlayerDealerTest.java
@@ -1,8 +1,7 @@
 package ru.nsu.dashkovskii;
 
-
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class PlayerDealerTest {
     @Test
