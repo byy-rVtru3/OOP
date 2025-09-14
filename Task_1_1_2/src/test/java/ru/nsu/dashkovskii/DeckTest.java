ce03d28b98f4d1378e0882f2c34859463e37fceb
Egor Dashkovskii <e.dashkovskii1@g.nsu.ru>
2025-09-14 16:35:56 +0700

    add deck tests

--- Task_1_1_2/src/test/java/ru/nsu/dashkovskii/DeckTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/dashkovskii/DeckTest.java
@@ -1,7 +1,37 @@
 package ru.nsu.dashkovskii;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DeckTest {
+
+    @Test
+    void oneDeckHas52Cards() {
+        Deck deck = new Deck(1);
+        assertEquals(52, deck.size());
+    }
+
+    @Test
+    void twoDecksHave104Cards() {
+        Deck deck = new Deck(2);
+        assertEquals(104, deck.size());
+    }
+
+    @Test
+    void dealCardReducesDeckSize() {
+        Deck deck = new Deck(1);
+        int before = deck.size();
+        deck.dealCard();
+        assertEquals(before - 1, deck.size());
+    }
+
+    @Test
+    void dealAllCardsLeavesZero() {
+        Deck deck = new Deck(1);
+        for (int i = 0; i < 52; i++) {
+            deck.dealCard();
+        }
+        assertEquals(0, deck.size());
+    }
 }
