b8a5e32e5e2af34b5e494334eb1dacbf4892af0e
Egor Dashkovskii <e.dashkovskii1@g.nsu.ru>
2025-09-23 14:08:29 +0700

    fix3 javadoc

--- Task_1_1_3/src/test/java/ru/nsu/dashkovskii/ParserTest.java
+++ Task_1_1_3/src/test/java/ru/nsu/dashkovskii/ParserTest.java
@@ -3,9 +3,7 @@ package ru.nsu.dashkovskii;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Тестовый класс для проверки функциональности класса Parser.
--- Task_1_1_3/src/test/java/ru/nsu/dashkovskii/ScannerTest.java
+++ Task_1_1_3/src/test/java/ru/nsu/dashkovskii/ScannerTest.java
@@ -2,10 +2,7 @@ package ru.nsu.dashkovskii;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Тестовый класс для проверки функциональности класса Scanner.
